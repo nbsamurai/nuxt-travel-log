d15702ac99690a9a594ee910f55d104875b12c6a
Subramani Simhachalam <subbu@live.in>
2025-11-23 10:52:18 +0530

    Github account login setup (#56)
    * Github account login setup
    
    * fix for missing github action secrets

---
+++ app/components/auth-button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { loading, signIn } = useAuthStore();
+</script>
+
+<template>
+  <button :disabled="loading" class="btn btn-accent" @click="signIn">
+    Sign In With Github
+    <span v-if="loading" class="loading loading-spinner loading-md" />
+    <Icon v-else name="tabler:brand-github" size="24" />
+  </button>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["./app/assets/css/main.css"],
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+    "@pinia/nuxt",
+  ],
   eslint: {
     config: {
       standalone: false,
