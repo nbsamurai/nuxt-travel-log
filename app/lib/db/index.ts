beec63177ded0344d29e7963bfff74f6dbc4e4b7
nbsamurai <subbu@live.in>
2025-11-24 09:23:33 +0530

    turso cloud login

--- app/lib/db/index.ts
+++ app/lib/db/index.ts
@@ -1,5 +1,5 @@
-// import { drizzle } from "drizzle-orm/libsql";
-import { drizzle } from "drizzle-orm/tursodatabase/database";
+import { drizzle } from "drizzle-orm/libsql";
+// import { drizzle } from "drizzle-orm/tursodatabase/database";
 
 import env from "../env";
 import * as schema from "./schema";
@@ -7,9 +7,9 @@ import * as schema from "./schema";
 // You can specify any property from the libsql connection options
 const db = drizzle({
   connection: {
-    // url: env.TURSO_DATABASE_URL,
-    // authToken: env.TURSO_AUTH_TOKEN,
-    path: env.DB_FILE_NAME,
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
+    // path: env.DB_FILE_NAME,
   },
   casing: "snake_case",
   schema,
