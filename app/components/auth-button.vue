c96633e537fb88f72eea835beee06831a7da5a0b
Subramani Simhachalam <subbu@live.in>
2025-11-24 11:42:27 +0530

    Tl 17 (#59)
    * Sign Out implemented
    
    * log out button implementation

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts">
-const { loading, signIn, user } = useAuthStore();
+const authStore = useAuthStore();
 </script>
 
 <template>
-  <div v-if="!loading && user" class="dropdown dropdown-end">
+  <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
     <div tabindex="0" role="button" class="btn m-1">
-      <div v-if="user.image" class="avatar">
+      <div v-if="authStore.user.image" class="avatar">
         <div class="w-8 rounded-full">
-          <img :src="user.image" :alt="user.name">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
         </div>
       </div>
-      {{ user.name }}
+
+      {{ authStore.user.name }}
     </div>
-    <ul tabindex="-1" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+
+    <ul tabindex="0" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
       <li>
         <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+
           Sign Out
         </NuxtLink>
       </li>
     </ul>
   </div>
-  <button v-else :disabled="loading" class="btn btn-accent" @click="signIn">
+
+  <button v-else :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
     Sign In With Github
-    <span v-if="loading" class="loading loading-spinner loading-md" />
+    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
     <Icon v-else name="tabler:brand-github" size="24" />
   </button>
 </template>
--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -5,7 +5,9 @@ const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("authStore", () => {
   const session = authClient.useSession();
+
   const user = computed(() => session.value.data?.user);
+
   const loading = computed(() => session.value.isPending || session.value.isRefetching);
   async function signIn() {
     await authClient.signIn.social({
