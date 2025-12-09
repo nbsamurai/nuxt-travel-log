76ae54fedc2f22a02162e0c88025bdfb8edaf412
nbsamurai <subbu@live.in>
2025-12-09 16:23:50 +0530

    Location Names / Slugs validated to be unique

--- app/lib/db/schema/auth.ts
+++ app/lib/db/schema/auth.ts
@@ -33,7 +33,7 @@ export const session = sqliteTable(
       .notNull(),
     ipAddress: text(),
     userAgent: text(),
-    userId: text()
+    userId: int()
       .notNull()
       .references(() => user.id, { onDelete: "cascade" }),
   },
@@ -46,7 +46,7 @@ export const account = sqliteTable(
     id: int().primaryKey({ autoIncrement: true }),
     accountId: text().notNull(),
     providerId: text().notNull(),
-    userId: text()
+    userId: int()
       .notNull()
       .references(() => user.id, { onDelete: "cascade" }),
     accessToken: text(),
