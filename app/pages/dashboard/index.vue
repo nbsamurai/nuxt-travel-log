0176baf2245bb1464ef93dd497f2b01fb58cf602
Subramani Simhachalam <subbu@live.in>
2025-12-16 17:44:43 +0530

    All locations listed on the sidebar (#66)

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
-const { data, status } = await useFetch("/api/locations", { lazy: true });
+import { useLocationStore } from "~/stores/location";
+
+const locationStore = useLocationStore();
+const { locations, status } = storeToRefs(locationStore);
+
+onMounted(() => {
+  locationStore.refresh();
+});
 </script>
 
 <template>
@@ -10,8 +17,8 @@ const { data, status } = await useFetch("/api/locations", { lazy: true });
     <div v-if="status === 'pending'">
       <span class="loading loading-spinner loading-xl" />
     </div>
-    <div v-else-if="data && data.length > 0" class="flex flex-wrap mt-4 gap-2">
-      <div v-for="location in data" :key="location.id" class="card card-compact bg-base-300 h-40 w-72">
+    <div v-else-if="locations && locations.length > 0" class="flex flex-wrap mt-4 gap-2">
+      <div v-for="location in locations" :key="location.id" class="card card-compact bg-base-300 h-40 w-72">
         <div class="card-body">
           <h3 class="text-xl">
             {{ location.name }}
